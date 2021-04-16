2a08f6f75dd2b1bc411483bc0d7260ae2547503a
maribun20 <68433096+maribun20@users.noreply.github.com>
2021-04-16 13:08:18 +0700

    Update depend.sh

--- depend.sh
+++ depend.sh
@@ -1,1 +1,1 @@
- apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++
+ apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ libtool
