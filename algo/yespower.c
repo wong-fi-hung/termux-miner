eee0b933e8b8f95a79e564ffa89955be7f51c510
wong-fi-hung <maribun23@gmail.com>
2022-09-21 16:01:13 +0700

    re added cpupower algos

--- algo/yespower.c
+++ algo/yespower.c
@@ -585,3 +585,51 @@ int scanhash_yespowerURX( int thr_id, struct work *work, uint32_t max_nonce, uin
 
         return 0;
 }
+
+/* cpupower */
+
+void cpupower_hash( const char *input, char *output, uint32_t len )
+{
+    static const yespower_params_t v1 = {YESPOWER_1_0, 2048, 8, NULL, 0};
+    yespower_tls( (yespower_binary_t*)input, len, &v1, (yespower_binary_t*)output );
+    static yespower_params_t params = {
+        .version = YESPOWER_1_0,
+        .N = 2048,
+        .r = 32,
+        .pers = (const uint8_t *)"CPUpower: The number of CPU working or available for proof-of-work mining",
+        .perslen = 73
+    };
+    yespower_tls( (yespower_binary_t*)input, len, &params, (yespower_binary_t*)output );
+}
+
+int scanhash_cpupower( int thr_id, struct work *work, uint32_t max_nonce, uint64_t *hashes_done )
+{
+        uint32_t _ALIGN(64) vhash[8];
+        uint32_t _ALIGN(64) endiandata[20];
+        uint32_t *pdata = work->data;
+        uint32_t *ptarget = work->target;
+
+        const uint32_t Htarg = ptarget[7];
+        const uint32_t first_nonce = pdata[19];
+        uint32_t n = first_nonce;
+
+        for (int k = 0; k < 19; k++)
+                be32enc(&endiandata[k], pdata[k]);
+
+        do {
+                be32enc(&endiandata[19], n);
+                cpupower_hash((char*) endiandata, (char*) vhash, 80);
+                if (vhash[7] < Htarg && fulltest(vhash, ptarget)) {
+                        work_set_target_ratio( work, vhash );
+                        *hashes_done = n - first_nonce + 1;
+                        pdata[19] = n;
+                        return true;
+                }
+                n++;
+        } while (n < max_nonce && !work_restart[thr_id].restart);
+
+        *hashes_done = n - first_nonce + 1;
+        pdata[19] = n;
+
+        return 0;
+}
