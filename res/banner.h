788008cac8f94da38c2eeba4afc85add18e3df17
wong-fi-hung <maribun23@gmail.com>
2023-02-21 14:43:53 +0700

    update res/banner.h

--- res/banner.h
+++ res/banner.h
@@ -16,9 +16,9 @@ static void show_credits()
         printf(CL_LCY"  Author  "CL_LYL"           : wong-fi-hung\n");
         printf(CL_LCY"  Telagram"CL_LYL"           : @Aminudin1 (Wong Fi Hung)\n");
         printf(CL_LCY"  Youtube Channel"CL_LYL"    : CHANNEL CRYPTO\n");
-        printf(CL_LCY"  Git repo"CL_LYL"           : https:" "/" "/" "github.com" "/" "maribun20" "/" "termux-miner\n");
+        printf(CL_LCY"  Git repo"CL_LYL"           : https:" "/" "/" "github.com" "/" "wong-fi-hung" "/" "termux-miner\n");
         printf(CL_LCY"  Original git repo"CL_LYL"  : https:" "/" "/" "github.com" "/" "tpruvot" "/" "cpuminer-multi\n");
-        printf(CL_LCY"  DGB donation addr"CL_LYL"  : DFnEipdeXYkUnvSby8pQ7jr8WJhQ4Rj9is (maribun20)\n");
+        printf(CL_LCY"  DGB donation addr"CL_LYL"  : DFnEipdeXYkUnvSby8pQ7jr8WJhQ4Rj9is (wong-fi-hung)\n");
         printf(CL_LCY"  BTC donation addr "CL_LYL" : 1FhDPLPpw18X4srecguG3MxJYe4a1JsZnd (tpruvot)\n");
         printf(CL_N"######################################################################\n");
 }
